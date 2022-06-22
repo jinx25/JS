4f7241d6943480683d603693c330e6f296b98369
jinx25 <michealngini@gmail.com>
2022-06-22 08:48:33 +0100

    Introduction to Javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+var firstName = "Micheal";
+var middleName = "Joel";
+var lastName = "Chukwunekwu";
+var height = "5'9ft";
+var country = "Nigeria"
+
+const fullName =  firstName + " " + middleName + " " + lastName
+
+document.write("Name: " + fullName + "<br/>")
+document.write("Height: " + height + "<br/>")
+document.write("Country: " + country + "<br/>")
